02aecdf0893d121b01d704c2fa6a0ee1b7edb327
KingArrkinian <weber.alexandre.pro@pm.me>
2021-09-30 16:57:56 +0200

    start check for a FAT drive

--- installer.py
+++ installer.py
@@ -1,10 +1,30 @@
-import os
 from platform import system
 import subprocess
+import os
+
+def is_on_FAT():
+    import psutil
+    drive = os.getcwd()[:2]
+    dps = psutil.disk_partitions()
+    for i in [0, len(dps) - 1]:
+        if (dps[i].device[:2] == drive):
+            if (dps[i].fstype.find("FAT") != -1):
+                print ("The current disk was detected as FAT.\nOpenEB cannot install on a FAT/exFAT drive. Please move and restart the script on a non-FAT partition.")
+                return(true)
+    return(false)
+                
+def has_psutil():
+    import importlib.util
+    return (importlib.util.find_spec('psutil') != "")
 
 if system == "Linux":
     subprocess.run(args=["./installer.sh"])
 elif system == "Windows":
-
+    if (has_psutil()):
+        if (is_on_FAT()):
+            exit()
+    else:
+        print("psutil not found: no check if disk is (ex)FAT or not (FAT drives will not work).")
+    
 else:
-    print("We only support Linux(Ubuntu) and Windows.")
+    print("We only support Ubuntu and Windows.")
