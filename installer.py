bd23b30d70b2bee9d3919505a3b656efd884b047
KingArrkinian <weber.alexandre.pro@pm.me>
2021-10-01 09:46:07 +0200

    installer end

--- installer.py
+++ installer.py
@@ -17,14 +17,26 @@ def has_psutil():
     import importlib.util
     return (importlib.util.find_spec('psutil') != "")
 
+#run the shell installer for Ubuntu
 if system == "Linux":
-    subprocess.run(args=["./installer.sh"])
+    subprocess.run(args=["installer.sh"])
+#run lots of subprocesses for Windows
 elif system == "Windows":
+    #check and exit if we are on a FAT/exFAT drive
     if (has_psutil()):
         if (is_on_FAT()):
             exit()
+    os.chdir("..")
+    #check for, and install a local vcpkg
+    if not os.isdir("vcpkg"):
+        subprocess.run(args=["git", "clone", "https://github.com/microsoft/vcpkg.git"])
+    os.chdir("vcpkg")
+    subprocess.run(args=["git", "checkout", "08c951fef9de63cde1c6b94245a63db826be2e32"])
+    subprocess.run(args=["bootstrap-vcpkg.bat"])
+    subprocess.run(args=["vcpkg.exe", "--triplet", "x64-windows", "install", "libusb", "eigen3", "boost", "opencv", "glfw3", "glew", "gtest", "dirent", "pybind11"])
     else:
-        print("psutil not found: no check if disk is (ex)FAT or not (FAT drives will not work).")
+        print("psutil not found: will not check if disk is (ex)FAT or not (FAT drives will not work).")
+    
     
 else:
     print("We only support Ubuntu and Windows.")
