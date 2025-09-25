5263c67616c4efc5455a8dfa7995cde8920cb4f4
Gokul B. Nair <gokulbnr@gmail.com>
2025-09-26 09:56:49 +1000

    attempts to make a conda package

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -ex
+
+# Create and enter build directory
+mkdir -p build
+cd build
+
+# Configure CMake
+cmake ${CMAKE_ARGS} \
+    -DCMAKE_INSTALL_PREFIX=$PREFIX \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SAMPLES=ON \
+    -DCOMPILE_PYTHON3_BINDINGS=ON \
+    -DBUILD_TESTING=OFF \
+    -DCODE_COVERAGE=OFF \
+    -DGENERATE_DOC=OFF \
+    ..
+
+# Build and install
+cmake --build . --target install -j${CPU_COUNT}
--- sdk/modules/core/cpp/src/utils/cd_frame_generator.cpp
+++ sdk/modules/core/cpp/src/utils/cd_frame_generator.cpp
@@ -68,9 +68,17 @@ void CDFrameGenerator::add_events(const EventCD *begin, const EventCD *end) {
     // Note: one could call frame_generation_algorithm->process_events directly but it may have a high overhead
     // depending on the inputs.and decreases the performance. Better ensure that bigger chunks of data are processed
     events_back_.insert(events_back_.end(), begin, end);
-    if (std::prev(end)->t > next_notify_us_) {
+    
+    // if (std::prev(end)->t > next_notify_us_) {
+    //    events_available_ = true;
+    //    next_notify_us_   = notify_slice_us_ * (1 + begin->t / notify_slice_us_);
+    //    events_available_cond_.notify_all();
+    //}
+
+    auto current_time = std::prev(end)->t;
+    if (current_time >= next_notify_us_) {
         events_available_ = true;
-        next_notify_us_   = notify_slice_us_ * (1 + begin->t / notify_slice_us_);
+        next_notify_us_   = notify_slice_us_; // * (1 + begin->t / notify_slice_us_);
         events_available_cond_.notify_all();
     }
 }
